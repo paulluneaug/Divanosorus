2748e7bb70b60f7238b190bce6ec58053b80d889
Corentin Boblet <corentinboblet@gmail.com>
2024-09-16 00:13:03 +0200

    Added utf-8 compability

--- divansorus_gameDatas/divansorus_datas.js
+++ divansorus_gameDatas/divansorus_datas.js
@@ -45,7 +45,7 @@ var udiskData =
 				{
 					"foldername": "RENCONTRE",
 					"displayName": "Ma première relation",
-					"files": ["Un après-midi sur l'eau.png", "Souvenir de sa maison.png", "Vacances avec elle.png", "Celle que j'aime.png"]
+					"files": ["Un apres-midi sur l'eau.png", "Souvenir de sa maison.png", "Vacances avec elle.png", "Celle que j'aime.png"]
 				},
 				{
 					"foldername": "DECHIRURE",
